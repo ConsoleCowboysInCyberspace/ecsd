ab2f9027ae04365ee81364909304c8cbbe0634d4
Steven Dwy <yoplitein+git@gmail.com>
2022-06-16 23:13:42 -0700

    Public import event modules, add function to register builtin components (just mailbox for now)

--- src/ecsd/package.d
+++ src/ecsd/package.d
@@ -2,5 +2,12 @@ module ecsd;
 
 public import ecsd.cache;
 public import ecsd.entity;
+public import ecsd.event.mailbox;
+public import ecsd.event.pubsub;
 public import ecsd.storage;
 public import ecsd.universe;
+
+void registerBuiltinComponents(Universe uni)
+{
+	uni.registerComponent!Mailbox;
+}
