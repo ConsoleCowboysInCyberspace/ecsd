41d4c7e903025994e9ce61088ac0c4601e78dbfc
Steven Dwy <yoplitein+git@gmail.com>
2022-06-16 22:45:45 -0700

    Grammar fix in isEvent message

--- src/ecsd/event/pubsub.d
+++ src/ecsd/event/pubsub.d
@@ -34,7 +34,7 @@ private:
 bool isEvent(T)()
 {
 	enum errPreamble = "Event type " ~ T.stringof ~ " must ";
-	static assert(is(T == struct), errPreamble ~ "be structs");
+	static assert(is(T == struct), errPreamble ~ "be a struct");
 	static assert(__traits(isPOD, T), errPreamble ~ "not have copy ctors/dtors");
 	static assert(__traits(compiles, { T x; }), errPreamble ~ "have a default constructor");
 	
