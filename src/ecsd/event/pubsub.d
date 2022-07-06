144748926c8567f5abdd69835c6b96e8768f61c2
Steven Dwy <yoplitein+git@gmail.com>
2022-07-06 15:47:40 -0700

    Add mixin template that automatically subscribes top level functions

--- src/ecsd/event/pubsub.d
+++ src/ecsd/event/pubsub.d
@@ -44,6 +44,58 @@ if(isEvent!Event)
 	publish(Event.init);
 }
 
+/// Attribute applied to functions that should be discovered by `registerSubscribers`.
+struct EventSubscriber
+{
+	int priority = 0;
+}
+
+/++
+	Generates a module constructor that will automatically subscribe all top-level functions (in
+	that module) marked with `EventSubscriber`.
+	
+	Examples:
+	---
+	mixin registerSubscribers;
+	
+	@EventSubscriber
+	private void subscriber1(ref Event ev) {}
+	
+	@EventSubscriber(-100)
+	private void subscriber2(ref Event ev) {}
+	---
++/
+mixin template registerSubscribers(string _targetModule = __MODULE__)
+{
+	static this()
+	{
+		import std.traits;
+		import std.meta;
+		import ecsd.event.pubsub;
+		
+		alias mod = mixin(_targetModule);
+		static foreach(symbol; getSymbolsByUDA!(mod, EventSubscriber))
+		{{
+			enum symbolName = fullyQualifiedName!symbol;
+			static assert(
+				isFunction!symbol,
+				"EventSubscriber attribute is restricted to function declarations, " ~
+				"but appears on " ~ symbolName
+			);
+			alias attrs = getUDAs!(symbol, EventSubscriber);
+			static assert(
+				attrs.length == 1,
+				"Duplicate EventSubscriber attribute on " ~ symbolName
+			);
+			
+			EventSubscriber inst;
+			static if(!is(attrs[0]))
+				inst = attrs[0];
+			subscribe(&symbol, inst.priority);
+		}}
+	}
+}
+
 private:
 
 struct EventHandler(Event)
@@ -57,21 +109,23 @@ template storage(Event)
 	EventHandler!Event[] storage;
 }
 
+version(unittest):
+
+struct TestEvent
+{
+	int x = -1;
+}
+
 unittest
 {
-	static struct Foo
-	{
-		int x = -1;
-	}
-	
 	bool calledf1, calledf2;
-	void f1(ref Foo)
+	void f1(ref TestEvent)
 	{
 		assert(!calledf1);
 		assert(calledf2);
 		calledf1 = true;
 	}
-	void f2(ref Foo ev)
+	void f2(ref TestEvent ev)
 	{
 		assert(!calledf1);
 		assert(!calledf2);
@@ -86,19 +140,47 @@ unittest
 	static void f3(ref Bar) { assert(false); }
 	subscribe(&f3);
 	
-	Foo ev = { 1 };
+	TestEvent ev = { 1 };
 	publish(ev);
 	assert(calledf1);
 	assert(calledf2);
+	assert(calledf1Toplevel);
+	assert(calledf2Toplevel);
 	assert(ev.x == 2);
 	calledf1 = calledf2 = false;
+	calledf1Toplevel = calledf2Toplevel = false;
 	
-	publish(Foo());
+	publish(TestEvent());
 	assert(calledf1);
 	assert(calledf2);
+	assert(calledf1Toplevel);
+	assert(calledf2Toplevel);
 	calledf1 = calledf2 = false;
+	calledf1Toplevel = calledf2Toplevel = false;
 	
-	publish!Foo;
+	publish!TestEvent;
 	assert(calledf1);
 	assert(calledf2);
+	assert(calledf1Toplevel);
+	assert(calledf2Toplevel);
 }
+
+bool calledf1Toplevel, calledf2Toplevel;
+
+@EventSubscriber
+void toplevelF1(ref TestEvent)
+{
+	assert(!calledf1Toplevel);
+	assert(calledf2Toplevel);
+	calledf1Toplevel = true;
+}
+
+@EventSubscriber(1)
+void toplevelF2(ref TestEvent ev)
+{
+	assert(!calledf1Toplevel);
+	assert(!calledf2Toplevel);
+	calledf2Toplevel = true;
+}
+
+mixin registerSubscribers;
